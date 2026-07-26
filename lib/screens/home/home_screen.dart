34b98f47b857c43d80bf6d9729f29a3eabf0ddd0
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-07-26 23:57:41 +0500

    Display live news articles

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../providers/news_provider.dart';
+import '../../widgets/article_card.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      Provider.of<NewsProvider>(context, listen: false).fetchNews();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<NewsProvider>(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text("News Reader"), centerTitle: true),
-      body: const Center(
-        child: Text(
-          "Home Screen",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+
+      body: Builder(
+        builder: (_) {
+          if (provider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (provider.error.isNotEmpty) {
+            return Center(child: Text(provider.error));
+          }
+
+          return ListView.builder(
+            itemCount: provider.articles.length,
+            itemBuilder: (context, index) {
+              return ArticleCard(article: provider.articles[index]);
+            },
+          );
+        },
       ),
     );
   }
