a63905ba7fc3d1052c6f73105d5db2b7459571fd
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-07-26 23:56:18 +0500

    Create article card widget

---
+++ lib/widgets/article_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../models/article.dart';
+
+class ArticleCard extends StatelessWidget {
+  final Article article;
+
+  const ArticleCard({super.key, required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      elevation: 5,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (article.imageUrl.isNotEmpty)
+            Image.network(
+              article.imageUrl,
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              article.title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(article.description),
+          ),
+
+          const SizedBox(height: 10),
+
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              article.source,
+              style: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
