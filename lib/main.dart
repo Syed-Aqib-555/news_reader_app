6b3bb8782bedcd6d4e86e0c1e66f060549b4f126
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-07-26 23:37:25 +0500

    Create basic application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const NewsReaderApp());
+}
+
+class NewsReaderApp extends StatelessWidget {
+  const NewsReaderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'News Reader',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('News Reader'), centerTitle: true),
+        body: const Center(
+          child: Text(
+            'Welcome to News Reader App',
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
